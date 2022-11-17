a5d5ec71c71197778eba3567212f0ed093b22252
Mateusz S <mstepinski98@gmail.com>
2022-11-17 13:45:03 +0100

    RandomizePosition method

--- Snake/Assets/Scripts/Food.cs
+++ Snake/Assets/Scripts/Food.cs
@@ -5,4 +5,20 @@ using UnityEngine;
 public class Food : MonoBehaviour
 {
     [SerializeField] BoxCollider2D foodSpawnArea;
+
+    void Start() 
+    {
+        RandomizePosition();
+    }
+
+    void RandomizePosition()
+    {
+        Bounds bounds = this.foodSpawnArea.bounds;
+
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+
+        //Rounding the values
+        this.transform.position = new Vector2(Mathf.Round(x), Mathf.Round(y));
+    }
 }
