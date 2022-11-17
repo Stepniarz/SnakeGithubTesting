77ba0e606dae871fcca567e7cc6d5ded25c49c64
Mateusz S <mstepinski98@gmail.com>
2022-11-17 14:16:27 +0100

    coding snake growth
    + some cleaning :)

--- Snake/Assets/Scripts/Snake.cs
+++ Snake/Assets/Scripts/Snake.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Snake : MonoBehaviour
 {
     Vector2 moveDirection = Vector2.up;
+    List<Transform> snakePartsList;
+    [SerializeField] Transform snakePartPrefab;
+
+    void Start() 
+    {
+        snakePartsList = new List<Transform>();
+        snakePartsList.Add(this.transform);
+    }
 
     void Update()
     {
@@ -33,9 +41,38 @@ public class Snake : MonoBehaviour
 
     void FixedUpdate() 
     {
+        SnakePartsFollow();
+        HeadMovement();
+        
+    }
+    void HeadMovement()
+    {
         this.transform.position = new Vector2(
             Mathf.Round(this.transform.position.x) + moveDirection.x,
             Mathf.Round(this.transform.position.y) + moveDirection.y
         );
+    }
+
+    void Grow()
+    {
+        Transform snakePart = Instantiate(this.snakePartPrefab);
+        snakePart.position = snakePartsList[snakePartsList.Count - 1].position;     //last position from the list
+        snakePartsList.Add(snakePart);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Food")
+        {
+            Grow();
+        }
+    }
+
+    void SnakePartsFollow()
+    {
+        for(int i = snakePartsList.Count - 1; i > 0; i--)
+        {
+            snakePartsList[i].position = snakePartsList[i - 1].position;
+        }
     }
 }
