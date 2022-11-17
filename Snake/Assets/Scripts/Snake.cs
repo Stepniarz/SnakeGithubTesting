5595ff0535a27e8fbb308ff5d7c8c46093613aac
Mateusz S <mstepinski98@gmail.com>
2022-11-17 13:12:58 +0100

    Movement method

--- Snake/Assets/Scripts/Snake.cs
+++ Snake/Assets/Scripts/Snake.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    Vector2 moveDirection = Vector2.up;
 
     void Update()
     {
-        
+       Movement(); 
+    }
+
+    void Movement()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            moveDirection = Vector2.up;
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            moveDirection = Vector2.down;
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            moveDirection = Vector2.left;
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            moveDirection = Vector2.right;
+        }
     }
 }
